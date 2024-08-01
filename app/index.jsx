15d601f500d157277ecdcaf316f24ab0d5f16011
DhanushkaTa <tharindu@leadinnovationz.com>
2024-08-01 20:22:34 +0530

    creating home

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View, Image } from 'react-native';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
+import { images } from '../constants';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      
-      <Text className="text-3xl font-pblack">Xmo</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{color: 'blue'}}>
-          Go to Home
-      </Link>
+    
+    <SafeAreaView className='bg-primary h-full' >
 
-      {/* <Text className="text-3xl text-red-500">Xmo</Text> */}
-      
-    </View>
+    <ScrollView contentContainerStyle={{ height: '100%'}}>
+
+      <View className="w-full justify-center items-center h-full px-4">
+        <Image
+          source={images.logo}
+          className="w-[130px] h-[84px]"
+          resizeMode='contain'
+        />
+
+        <Image 
+          source={images.cards}
+          className="max-w-[380px] w-full h-[300px]"
+          resizeMode='contain'
+        />
+
+        <View>
+          <Text className="text-white text-3xl font-bold text-center">
+            Discover Endless Possibilities with{' '}
+            <Text className="text-secondary-200">
+              Xmo
+            </Text>
+
+          </Text>
+
+          <Image
+              source={images.path}
+              className="w-[136px] h-[12px] absolute -bottom-2 -right-8"
+              resizeMode='contain'
+          />
+
+          <Text className="text-gray-100 font-pregular text-sm mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Xmo
+          </Text>
+        </View>
+
+      </View>
+
+    </ScrollView>
+
+    </SafeAreaView>
+
   );
 }
 
