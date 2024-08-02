e52a10740c6f0fcdf786b68edc00e46f84b72bfb
DhanushkaTa <tharindu@leadinnovationz.com>
2024-08-02 21:15:49 +0530

    create custome hook

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,20 +1,24 @@
-import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from '@/constants';
 import SearchInput from '@/components/SearchInput';
 import Trending from '@/components/Trending';
 import EmptyState from '@/components/EmptyState';
+import { getAllPosts } from '@/lib/appwrite';
+import useAppwrite from '@/lib/useAppwrite';
 
 const Home = () => {
 
+  const { data: posts , refetch} = useAppwrite(getAllPosts)
+
   const [refreshing, setRefreshing] = useState(false);
 
-  const onRefresh = () => {
+  const onRefresh = async () => {
     setRefreshing(true);
     // Fetch data from API and update the state
-
+    await refetch();
     setRefreshing(false);
   }
 
