3b28da921fdc1a3c9090336bd40e304315e00d93
DhanushkaTa <tharindu@leadinnovationz.com>
2024-08-02 08:49:48 +0530

    complete sign up

--- app/(auth)/sing-up.jsx
+++ app/(auth)/sing-up.jsx
@@ -1,7 +1,7 @@
 import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Link } from 'expo-router';
+import { Link, router } from "expo-router";
 
 import { images } from '@/constants';
 import FormField from '@/components/FormField';
@@ -32,8 +32,8 @@ const SingUp = () => {
 
     try {
       const result = await saveUser(form.email, form.password, form.username);
-      setUser(result);
-      setIsLogged(true);
+      // setUser(result);
+      // setIsLogged(true);
 
       router.replace("/home");
     } catch (error) {
